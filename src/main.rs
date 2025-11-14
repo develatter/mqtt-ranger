02fbc5680cc420e9c37452ed5060597ddefef8eb
Lattecast <rattack.hyde@gmail.com>
2025-11-14 12:22:33 +0100

    Refactor and componentization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use std::time::{Duration, Instant};
+
+pub mod app;
+pub mod tui;
+use app::{AppState as App, run_app};
+use crossterm::{
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{Terminal, backend::CrosstermBackend};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let tick_rate = Duration::from_millis(250);
+
+    let mut app = App::new();
+
+    let res = run_app(&mut terminal, &mut app, tick_rate);
+
+    disable_raw_mode()?;
+    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        eprintln!("Error: {err}");
+    }
+
+    Ok(())
 }
